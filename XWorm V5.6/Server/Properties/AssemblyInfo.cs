5ee0420d74bc8c9e95e57931dfa8ddb76cb151c6
Denis T. <deadspell21391@gmail.com>
2025-10-22 00:15:48 +0200

    AssemblyInfo.cs

---
+++ XWorm V5.6/Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XWorm")]
+[assembly: AssemblyDescription("XCoder")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XWorm")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7DBADAA8-45F8-4326-9CE3-AA0C28FECFBA")]
+[assembly: AssemblyFileVersion("7.14.303.5690")]
+[assembly: AssemblyVersion("7.14.303.5690")]
